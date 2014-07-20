0347d47087dea0a9896a62d30fcbf88543cc25ba
Andrey Zholos <aaz@q-fu.com>
2014-07-20 14:24:08 +0000

    update <sys/wait.h> patch

--- graphics/povray37-devel/files/patch-vfe_unix_vfeplatform.cpp
+++ graphics/povray37-devel/files/patch-vfe_unix_vfeplatform.cpp
@@ -1,10 +1,10 @@
---- vfe/unix/vfeplatform.cpp.orig	2011-01-02 14:43:05.000000000 +0100
-+++ vfe/unix/vfeplatform.cpp	2011-03-08 12:57:50.000000000 +0100
-@@ -97,6 +97,7 @@
- #include <pthread.h>
- #include <stdlib.h>
- #include <string.h>
+--- vfe/unix/vfeplatform.cpp
++++ vfe/unix/vfeplatform.cpp
+@@ -50,6 +50,7 @@
+ #ifdef HAVE_SYS_TIME_H
+ # include <sys/time.h>
+ #endif
 +#include <sys/wait.h>
  
- #ifdef HAVE_TIME_H
- # include <time.h>
+ // from directory "vfe"
+ #include "vfe.h"
