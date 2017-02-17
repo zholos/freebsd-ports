a9e248cefff20707c5990f0df76e653ab5ab7096
Andrey Zholos <aaz@q-fu.com>
2017-02-17 16:11:57 +0000

    update to latest beta release

--- graphics/povray37-devel/files/patch-vfe_unix_syspovconfig_bsd.h
+++ graphics/povray37-devel/files/patch-vfe_unix_syspovconfig_bsd.h
@@ -3,10 +3,10 @@
 @@ -39,16 +39,18 @@
  #ifndef POVRAY_UNIX_SYSPOVCONFIG_BSD_H
  #define POVRAY_UNIX_SYSPOVCONFIG_BSD_H
-
+ 
 -#include <unistd.h>
 +#include <limits.h>
-
+ 
 -#if defined(_POSIX_V6_LPBIG_OFFBIG) || defined(_POSIX_V6_LP64_OFF64) || defined(_POSIX_V6_ILP32_OFFBIG)
 +#ifdef OFF_MAX
 +#if OFF_MAX >= 0x7fffffffffffffffLL
@@ -16,14 +16,14 @@
 +#else
      // off_t is 32 bits
      // Comment-out the following line to proceed anyway.
-     #error Image size will be limited to approx. 100 Megapixels. Proceed at your own risk.
+     #error "Image size will be limited to approx. 100 Megapixels. Proceed at your own risk."
      #define lseek64(handle,offset,whence) lseek(handle,offset,whence)
 +#endif
  #else
      // Unable to detect off_t size at compile-time; comment-out the following line to proceed anyway.
-     #error Image size may be limited to approx. 100 Megapixels. Proceed at your own risk.
+     #error "Image size may be limited to approx. 100 Megapixels. Proceed at your own risk."
 @@ -57,14 +59,11 @@
-
+ 
  /// @file
  /// @todo The POV_LONG stuff is just copied from the Posix settings; someone needs to verify universal BSD compatibility.
 -#if defined(_POSIX_V6_LPBIG_OFFBIG) || defined(_POSIX_V6_LP64_OFF64)
@@ -38,3 +38,4 @@
 +    // long is 32 bits.
      #define POV_LONG long long
  #endif
+ 
